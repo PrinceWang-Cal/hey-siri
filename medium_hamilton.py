625adbcb3bc21a137f77cb3801ed650a23caab58
Sabrina Zhao <sabrinazhao@Sabrinas-MacBook-Pro.local>
2019-11-22 04:19:02 -0800

    dense graph

---
+++ medium_hamilton.py
@@ -0,0 +1,13 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+#Dense group with many paths but only one is ideal(break dij/sp)
+#pick path with both home and normal locations
+def dense_graph():
+    G = nx.gnp_random_graph(25, 0.4)
+    G = nx.relabel_nodes(G, lambda x: 58 + x);
+    return G
+
+G = dense_graph()
+nx.draw_networkx(G)
+plt.show()
